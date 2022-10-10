5bf422a129d4cf22d3f40c1a5c0deb66cec5e150
azotamiota <azotamiota@gmail.com>
2022-10-10 23:53:06 +0100

    add solution that passes timeout limit

--- rotate-array/index.js
+++ rotate-array/index.js
@@ -1,3 +1,14 @@
 var rotate = function(nums, k) {
+    if (nums.length > k) {
+        const popped = []
+        for (let i = 0; i < k; i++) {        
+            popped.push(nums.pop())
+        }
+        nums.unshift(...popped.reverse())
+    } else {
+        for (let i = 0; i < k; i++) {        
+            nums.unshift(nums.pop())
+        }
+    }
     
 };
