57e95a31b22e4c6d608620cda0fa43e68e211c8d
azotamiota <azotamiota@gmail.com>
2022-10-14 18:07:22 +0100

    add solution

---
+++ first-bad-version/index.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let result = true
+        while (result) {
+            result = isBadVersion(n)
+            n--
+        }
+        return n + 2
+    };
+    
+};
